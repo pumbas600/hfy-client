33bc1f5ad2227db292c2522f545b6933d5ad6ff0
pumbas600 <joshjeffers600@gmail.com>
2024-08-30 16:42:55 +1200

    feat: add fontawesome and replace read on Reddit with icon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import "@/styles/styles.css";
 
+// See: https://docs.fontawesome.com/web/use-with/react/use-with#getting-font-awesome-css-to-work
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 export const metadata: Metadata = {
   title: "HFY Client",
   description: "A reddit client for browsing the r/HFY subreddit.",
--- frontend/src/components/layout/chapterHeader/index.tsx
+++ frontend/src/components/layout/chapterHeader/index.tsx
@@ -1,7 +1,8 @@
 import { FullChapter } from "@/types/chapter";
 import PageHeader from "../pageHeader";
 import styles from "./chapterHeader.module.css";
-import SubtleLink from "@/components/links/subtleLink";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReddit } from "@fortawesome/free-brands-svg-icons/faReddit";
 
 export interface ChapterHeaderProps {
   chapter: FullChapter;
@@ -10,13 +11,20 @@ export interface ChapterHeaderProps {
 export default function ChapterHeader({ chapter }: ChapterHeaderProps) {
   return (
     <PageHeader>
-      <div className={styles.authorContainer}>
-        <strong>r/{chapter.subreddit}</strong>
-        <a href={chapter.redditAuthorLink}>{chapter.author}</a>
+      <div className={styles.detailsContainer}>
+        <div className={styles.authorContainer}>
+          <strong>r/{chapter.subreddit}</strong>
+          <a href={chapter.redditAuthorLink}>{chapter.author}</a>
+        </div>
+        <a href={chapter.redditPostLink} title="Read on Reddit">
+          <FontAwesomeIcon
+            icon={faReddit}
+            size="lg"
+            className={styles.redditIcon}
+          />
+        </a>
       </div>
       <h2>{chapter.title}</h2>
-      <a href={chapter.redditPostLink}>Read on Reddit</a>
-
       <time>{/*Created at ...*/}</time>
     </PageHeader>
   );
