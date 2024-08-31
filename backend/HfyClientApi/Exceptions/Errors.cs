ac962942c816ae459931db90da86bbff83a16888
pumbas600 <joshjeffers600@gmail.com>
2024-08-31 22:04:44 +1200

    refactor: get self post using SelfPost(fullname).About() method

--- backend/HfyClientApi/Exceptions/Errors.cs
+++ backend/HfyClientApi/Exceptions/Errors.cs
@@ -9,7 +9,6 @@ namespace HfyClientApi.Exceptions
     public static class Codes
     {
       public const string PostNotFound = "Post.NotFound";
-      public const string PostNotSelf = "Post.NotSelfPost";
       public const string ChapterNotFound = "Chapter.NotFound";
       public const string ChapterUpsertFailed = "Chapter.UpsertFailed";
 
@@ -17,8 +16,6 @@ namespace HfyClientApi.Exceptions
 
     public static Error PostNotFound(string postId) =>
       new(Codes.PostNotFound, $"No post found for id {postId}", HttpStatusCode.NotFound);
-    public static Error PostNotSelfPost(string postId) =>
-      new(Codes.PostNotSelf, $"The post with id {postId} is not a text post", HttpStatusCode.BadRequest);
 
     public static Error ChapterNotFound(string chapterId) =>
       new(Codes.ChapterNotFound, $"No chapter found for id {chapterId}", HttpStatusCode.NotFound);
--- backend/HfyClientApi/Services/RedditService.cs
+++ backend/HfyClientApi/Services/RedditService.cs
@@ -2,42 +2,28 @@ using HfyClientApi.Exceptions;
 using HfyClientApi.Utils;
 using Reddit;
 using Reddit.Controllers;
-using Reddit.Exceptions;
 
 namespace HfyClientApi.Services
 {
   public class RedditService : IRedditService
   {
-    private readonly RedditClient _reddit;
+    private readonly RedditClient _redditClient;
 
     public RedditService(RedditClient redditClient)
     {
-      _reddit = redditClient;
+      _redditClient = redditClient;
     }
 
     public Result<SelfPost> GetSelfPostById(string postId)
     {
-      try
-      {
-        var postFullname = $"t3_{postId}";
-        var posts = _reddit.GetPosts([postFullname]);
-        if (posts.Count == 0)
-        {
-          return Errors.PostNotFound(postId);
-        }
-
-        var post = posts[0];
-        if (post is not SelfPost selfPost)
-        {
-          return Errors.PostNotSelfPost(postId);
-        }
-
-        return selfPost;
-      }
-      catch (RedditNotFoundException)
+      var postFullname = $"t3_{postId}";
+      var selfPost = _redditClient.SelfPost(postFullname).About();
+      if (selfPost == null)
       {
         return Errors.PostNotFound(postId);
       }
+
+      return selfPost;
     }
   }
 }
