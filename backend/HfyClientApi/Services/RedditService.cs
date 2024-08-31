e4fccf1b7e70633fe0cf0e111d541336ba9b467a
pumbas600 <joshjeffers600@gmail.com>
2024-08-31 21:52:00 +1200

    feat: handle reddit not found exception

--- backend/HfyClientApi/Services/RedditService.cs
+++ backend/HfyClientApi/Services/RedditService.cs
@@ -2,6 +2,7 @@ using HfyClientApi.Exceptions;
 using HfyClientApi.Utils;
 using Reddit;
 using Reddit.Controllers;
+using Reddit.Exceptions;
 
 namespace HfyClientApi.Services
 {
@@ -16,20 +17,27 @@ namespace HfyClientApi.Services
 
     public Result<SelfPost> GetSelfPostById(string postId)
     {
-      var postFullname = $"t3_{postId}";
-      var posts = _reddit.GetPosts([postFullname]);
-      if (posts.Count == 0)
+      try
+      {
+        var postFullname = $"t3_{postId}";
+        var posts = _reddit.GetPosts([postFullname]);
+        if (posts.Count == 0)
+        {
+          return Errors.PostNotFound(postId);
+        }
+
+        var post = posts[0];
+        if (post is not SelfPost selfPost)
+        {
+          return Errors.PostNotSelfPost(postId);
+        }
+
+        return selfPost;
+      }
+      catch (RedditNotFoundException)
       {
         return Errors.PostNotFound(postId);
       }
-
-      var post = posts[0];
-      if (post is not SelfPost selfPost)
-      {
-        return Errors.PostNotSelfPost(postId);
-      }
-
-      return selfPost;
     }
   }
 }
