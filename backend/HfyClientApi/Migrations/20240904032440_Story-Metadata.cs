1845c88b7375570830a56a3bd4ef9f188f9eb90d
pumbas600 <joshjeffers600@gmail.com>
2024-09-04 15:26:31 +1200

    chore: add migration for story metadata

---
+++ backend/HfyClientApi/Migrations/20240904032440_Story-Metadata.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HfyClientApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class StoryMetadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProcessedAtUtc",
+                table: "Chapters",
+                newName: "SyncedAtUtc");
+
+            migrationBuilder.CreateTable(
+                name: "StoryMetadata",
+                columns: table => new
+                {
+                    FirstChapterId = table.Column<string>(type: "text", nullable: false),
+                    CoverArtUrl = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryMetadata", x => x.FirstChapterId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chapters_Subreddit",
+                table: "Chapters",
+                column: "Subreddit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StoryMetadata");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chapters_Subreddit",
+                table: "Chapters");
+
+            migrationBuilder.RenameColumn(
+                name: "SyncedAtUtc",
+                table: "Chapters",
+                newName: "ProcessedAtUtc");
+        }
+    }
+}
