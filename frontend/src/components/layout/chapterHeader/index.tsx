703b075946de5d62c483a980c8dea3138af3b60d
pumbas600 <joshjeffers600@gmail.com>
2024-09-06 22:55:34 +1200

    feat: add upvotes to the chapter

--- frontend/src/components/layout/chapterHeader/index.tsx
+++ frontend/src/components/layout/chapterHeader/index.tsx
@@ -4,6 +4,7 @@ import styles from "./chapterHeader.module.css";
 import ChapterTimeMetadata from "@/components/chapterTimeMetadata";
 import { UnderlinedLink } from "@/components/links";
 import CoverArt from "@/components/images/coverArt";
+import Upvotes from "@/components/upvotes";
 
 export interface ChapterHeaderProps {
   chapter: FullChapter;
@@ -32,10 +33,13 @@ export default function ChapterHeader({ chapter }: ChapterHeaderProps) {
           </UnderlinedLink>
         </div>
         <h2 className={styles.title}>{chapter.title}</h2>
-        <ChapterTimeMetadata
-          createdAtUtc={chapter.createdAtUtc}
-          syncedAtUtc={chapter.syncedAtUtc}
-        />
+        <div className={styles.metadataContainer}>
+          <Upvotes upvotes={chapter.upvotes} downvotes={chapter.downvotes} />
+          <ChapterTimeMetadata
+            createdAtUtc={chapter.createdAtUtc}
+            syncedAtUtc={chapter.syncedAtUtc}
+          />
+        </div>
       </div>
     </PageHeader>
   );
