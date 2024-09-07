f4403111f2707ba4834493b3f003a30dd28dba29
pumbas600 <joshjeffers600@gmail.com>
2024-09-08 00:27:59 +1200

    chore: add migration for new searchable title column

---
+++ backend/HfyClientApi/Migrations/20240907120538_Searchable-Title.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HfyClientApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SearchableTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SearchableTitle",
+                table: "Chapters",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SearchableTitle",
+                table: "Chapters");
+        }
+    }
+}
