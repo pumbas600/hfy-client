516b4de9faa95ffe8fae88b47631d8d3bfd0729b
pumbas600 <joshjeffers600@gmail.com>
2024-09-12 23:39:34 +1200

    chore: create migration

---
+++ backend/HfyClientApi/Migrations/20240912113854_Subreddit-Model.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HfyClientApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubredditModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subreddits",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    IconUrl = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subreddits", x => x.Name);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subreddits");
+        }
+    }
+}
