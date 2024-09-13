6da5c9d292bccbc375ab0c38f03a2d1802a6bdf8
pumbas600 <joshjeffers600@gmail.com>
2024-09-14 01:37:19 +1200

    feat: update search query if more than 2 characters change

--- frontend/src/components/subreddit/chapterSearchInput/index.tsx
+++ frontend/src/components/subreddit/chapterSearchInput/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./chapterSearchInput.module.css";
 
 export interface CharacterSearchInputProps
@@ -16,13 +16,10 @@ export default function ChapterSearchInput({
   subreddit,
   ...props
 }: CharacterSearchInputProps) {
+  const queryRef = useRef<string>("");
   const router = useRouter();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const searchQuery = formData.get("q")?.toString().trim();
-
+  const handleQuery = (searchQuery?: string): void => {
     if (searchQuery === undefined || searchQuery === "") {
       router.push(`/r/${subreddit}`);
       return;
@@ -32,6 +29,24 @@ export default function ChapterSearchInput({
     router.push(searchUrl);
   };
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const searchQuery = formData.get("q")?.toString().trim();
+    handleQuery(searchQuery);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const searchQuery = event.target.value;
+    const lengthDiff = searchQuery.length - queryRef.current.length;
+
+    if (Math.abs(lengthDiff) >= 2) {
+      handleQuery(searchQuery.trim());
+    }
+
+    queryRef.current = searchQuery;
+  };
+
   return (
     <form role="search" onSubmit={handleSubmit}>
       <input
@@ -39,6 +54,7 @@ export default function ChapterSearchInput({
         name="q"
         placeholder="Search chapters..."
         aria-label="Search for chapters by their title"
+        onChange={handleChange}
         {...props}
         className={`${styles.searchInput} ${props.className ?? ""}`}
       />
