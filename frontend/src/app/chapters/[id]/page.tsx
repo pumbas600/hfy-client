de25f87aac4fed0b3bb783075d8ca6b96f23d20c
pumbas600 <joshjeffers600@gmail.com>
2024-09-15 22:41:16 +1200

    chore: delete old chapter and page header

--- frontend/src/app/chapters/[id]/page.tsx
+++ frontend/src/app/chapters/[id]/page.tsx
@@ -1,8 +1,5 @@
 import ChapterButtons from "@/components/buttons/chapterButtons";
 import ScrollToTopButton from "@/components/buttons/scrollToTopButton";
-import Container from "@/components/atomic/container";
-import ChapterHeader from "@/components/layout/chapterHeader";
-import PageFooter from "@/components/layout/pageFooter";
 import TextLayout from "@/components/layout/textLayout";
 import config from "@/config";
 import { GetChapterRequest } from "@/types/api";
