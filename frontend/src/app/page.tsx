b52fdde1852e89642b0c0cdcc0f728b1f48e92c1
pumbas600 <joshjeffers600@gmail.com>
2024-09-15 19:50:23 +1200

    chore: change redirect from / to /r/hfy to be permanent

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/r/HFY");
+  permanentRedirect("/r/HFY");
 }
