200314367c90b2f658547742173390ea092885b7
pumbas600 <joshjeffers600@gmail.com>
2024-09-17 20:43:17 +1200

    refactor: create separate components for the different regions

---
+++ frontend/src/components/layout/pageLayout/regions.tsx
@@ -0,0 +1,49 @@
+import styles from "./pageLayout.module.css";
+
+export interface RegionProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export interface StickyProps {
+  start?: React.ReactNode;
+  content?: React.ReactNode;
+  end?: React.ReactNode;
+  className?: string;
+}
+
+export function Sticky({ start, content, end, className }: StickyProps) {
+  return (
+    <div className={`${styles.sticky} ${className ?? ""}`}>
+      <div className={styles.stickyContent}>
+        {start ?? <div />}
+        {content ?? <div />}
+        {end ?? <div />}
+      </div>
+    </div>
+  );
+}
+
+export function Header({ children, className }: RegionProps) {
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.content} ${className ?? ""}`}>{children}</div>
+    </header>
+  );
+}
+
+export interface MainProps extends RegionProps {
+  noInlinePadding?: boolean;
+}
+
+export function Main({ children, className, noInlinePadding }: MainProps) {
+  const mainPaddingClassName = noInlinePadding ? "" : styles.mainPadding;
+
+  return (
+    <main
+      className={`${styles.main} ${mainPaddingClassName} ${className ?? ""}`}
+    >
+      {children}
+    </main>
+  );
+}
