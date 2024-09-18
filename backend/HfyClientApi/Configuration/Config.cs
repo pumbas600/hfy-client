dab7a4ecd745408bc9b9053f7a3b26de4886b26d
pumbas600 <joshjeffers600@gmail.com>
2024-09-18 21:22:38 +1200

    fix: user agent causing parsing error

--- backend/HfyClientApi/Configuration/Config.cs
+++ backend/HfyClientApi/Configuration/Config.cs
@@ -15,7 +15,7 @@ namespace HfyClientApi.Configuration
       public const string NoRedirect = "NoRedirect";
     }
 
-    public const string UserAgent = "web.application:hfy.client:v0.0.1";
+    public const string UserAgent = "hfy-client/v0-0-1";
     public const string RedditUrl = "https://www.reddit.com";
     public const string OldRedditUrl = "https://old.reddit.com";
     public const string OauthRedditUrl = "https://oauth.reddit.com";
