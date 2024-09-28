3af80d9e3892c3e6c35fc47ff8df1ab620fc5850
pumbas600 <joshjeffers600@gmail.com>
2024-09-28 14:01:04 +1200

    fix: set theme key in get theme script

--- frontend/src/config/getTheme.ts
+++ frontend/src/config/getTheme.ts
@@ -1,9 +1,11 @@
+import { LocalStorageKeys } from "./localStorage";
+
 export type Theme = "light" | "dark" | "system";
 export type ResolvedTheme = Exclude<Theme, "system">;
 
 const code = function () {
-  // These have to be defined here because the function gets turned into a string.
-  const ThemeKey = "hfy.theme";
+  // We replace this string with the actual theme key.
+  const ThemeKey = "hfy:theme";
   let theme: Theme = "system";
   let resolvedTheme: ResolvedTheme = "dark";
   try {
@@ -22,7 +24,11 @@ const code = function () {
     resolvedTheme = theme;
   }
 
+  console.log("[Theme] Resolved theme:", resolvedTheme);
   document.documentElement.dataset.theme = resolvedTheme;
 };
 
-export const getTheme = `(${code})();`;
+export const getTheme = `(${code})();`.replace(
+  "hfy:theme",
+  LocalStorageKeys.theme
+);
