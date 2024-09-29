aec4986153ffd12fd86a3987586d4c038c905db9
pumbas600 <joshjeffers600@gmail.com>
2024-09-29 23:53:21 +1300

    feat: create middleware for protecting authorized routes

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,17 @@
+import appConfig from "@/config";
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest): NextResponse {
+  const isAuthenticated = request.cookies.has(appConfig.cookies.refreshToken);
+  console.debug(`[Middleware]: User is authenticated: ${isAuthenticated}`);
+
+  if (!isAuthenticated) {
+    return NextResponse.redirect("/login");
+  }
+
+  return NextResponse.next();
+}
+
+export const config: MiddlewareConfig = {
+  matcher: ["/r/:path*", "/chapters/:path*", "/settings"],
+};
