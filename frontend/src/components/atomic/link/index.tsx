92c1b13ffd76ca88d0b1aa6dadcb672a066deb97
pumbas600 <joshjeffers600@gmail.com>
2024-10-01 23:08:19 +1300

    feat: add icon button support to link

--- frontend/src/components/atomic/link/index.tsx
+++ frontend/src/components/atomic/link/index.tsx
@@ -1,18 +1,24 @@
 import NextLink from "next/link";
 import styles from "./links.module.css";
+import iconButtonStyles from "../iconButton/iconButton.module.css";
 import { ReactNode } from "react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export interface BaseLinkProps {
   className?: string;
   children?: ReactNode;
   href: string;
   title?: string;
+  icon?: IconProp;
   variant?: "subtle" | "underlined" | "button" | "iconButton";
 }
 
 export default function Link({
   className,
+  children,
   variant = "underlined",
+  icon,
   ...props
 }: BaseLinkProps) {
   let variantClassName = "";
@@ -27,16 +33,22 @@ export default function Link({
       variantClassName = `button ${styles.buttonLink}`;
       break;
     case "iconButton":
-      variantClassName = `button ${styles.iconButtonLink}`;
-      break;
+      variantClassName = `button ${styles.buttonLink} ${iconButtonStyles.iconButton}`;
+      if (icon) {
+        children = (
+          <>
+            <FontAwesomeIcon icon={icon} size="xl" />
+            {children}
+          </>
+        );
+      }
   }
 
   if (props.href?.startsWith("/")) {
     return (
-      <NextLink
-        {...props}
-        className={`${variantClassName} ${className ?? ""}`}
-      />
+      <NextLink {...props} className={`${variantClassName} ${className ?? ""}`}>
+        {children}
+      </NextLink>
     );
   }
 
@@ -45,6 +57,8 @@ export default function Link({
       target="_blank"
       {...props}
       className={`${variantClassName} ${className ?? ""}`}
-    />
+    >
+      {children}
+    </a>
   );
 }
