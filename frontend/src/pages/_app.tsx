6ec1fe321db0f02ddbca09c59b7db80583439082
pumbas600 <joshjeffers600@gmail.com>
2024-10-06 00:09:37 +1300

    refactor: move / page from app router to pages router

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,11 @@ import { AppProps } from "next/app";
 
 import "@/styles/styles.css";
 
+// See: https://docs.fontawesome.com/web/use-with/react/use-with#getting-font-awesome-css-to-work
+import { config as fontAwesomeConfig } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+fontAwesomeConfig.autoAddCss = false;
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
