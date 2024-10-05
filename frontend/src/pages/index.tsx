48c54a58ab1ec539589732f2ee1f60ada43315b2
pumbas600 <joshjeffers600@gmail.com>
2024-10-06 01:08:22 +1300

    feat: change card depending on auth status

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,38 +1,47 @@
-import { Card, HeadMeta, Link, Text } from "@/components/atomic";
-import { Header, Main, Sticky } from "@/components/layout/pageLayout";
+import { HeadMeta, Text } from "@/components/atomic";
+import { Main, Sticky } from "@/components/layout/pageLayout";
 import PrimaryLayout from "@/components/layout/primaryLayout";
 import config from "@/config";
-import styles from "@/components/home/home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons/faArrowRight";
 import { WhitelistMessage } from "@/config/constants";
 import LoginCard from "@/components/loginAndAuthorize/loginCard";
+import { getSelf } from "@/lib/getSelf";
 
 export default function Home() {
+  const isAuthenticated = getSelf() !== undefined;
+
   return (
     <>
       <HeadMeta title={config.title} description={config.description} />
       <PrimaryLayout>
-        <Sticky>
-          <div>
-            <h4>{config.title}</h4>
-            <Text color="primaryContrastSecondary">
-              Optimizing your Reddit reading experience.
-            </Text>
-          </div>
-        </Sticky>
+        <Sticky />
         <Main noInlinePadding>
-          <LoginCard
-            title="Beta Access"
-            primaryLinkUrl="/login"
-            primaryLinkChildren={
-              <>
-                Login <FontAwesomeIcon size="lg" icon={faArrowRight} />
-              </>
-            }
-          >
-            {WhitelistMessage}
-          </LoginCard>
+          {isAuthenticated ? (
+            <LoginCard
+              title="Start Reading"
+              primaryLinkUrl="/r/HFY"
+              primaryLinkChildren={
+                <>
+                  Go to r/HFY <FontAwesomeIcon size="lg" icon={faArrowRight} />
+                </>
+              }
+            >
+              Welcome to the {config.title} beta! Go to r/HFY to start reading.
+            </LoginCard>
+          ) : (
+            <LoginCard
+              title="Beta Access"
+              primaryLinkUrl="/login"
+              primaryLinkChildren={
+                <>
+                  Login <FontAwesomeIcon size="lg" icon={faArrowRight} />
+                </>
+              }
+            >
+              {WhitelistMessage}
+            </LoginCard>
+          )}
         </Main>
       </PrimaryLayout>
     </>
