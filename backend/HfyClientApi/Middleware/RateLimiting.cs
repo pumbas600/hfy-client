a7e7a51118ce829faf85a9a34ed666baf74dc460
pumbas600 <joshjeffers600@gmail.com>
2024-10-08 23:32:54 +1300

    feat: get options for different rate limiters

--- backend/HfyClientApi/Middleware/RateLimiting.cs
+++ backend/HfyClientApi/Middleware/RateLimiting.cs
@@ -1,16 +1,22 @@
-using System.Net;
 using System.Threading.RateLimiting;
 
 namespace HfyClientApi.Middleware
 {
-  public static class RateLimitingPolicies
+  public static class RateLimiterPolicies
   {
     public const string Public = "Public";
     public const string PublicLogin = "PublicLogin";
     public const string Authenticated = "Authenticated";
   }
 
-  public class RateLimitingOptions
+  public static class RateLimiterSections
+  {
+    public const string Public = "RateLimiter:Public";
+    public const string PublicLogin = "RateLimiter:PublicLogin";
+    public const string Authenticated = "RateLimiter:Authenticated";
+  }
+
+  public class RateLimiterOptions
   {
     public int PermitLimit { get; set; }
     public int WindowSeconds { get; set; }
@@ -18,14 +24,23 @@ namespace HfyClientApi.Middleware
     public int QueueLimit { get; set; }
   }
 
-  public class RateLimiting
+  public class RateLimiter
   {
-    public static void ConfigureRateLimiting(IServiceCollection services)
+    public static void ConfigureRateLimiting(IConfiguration configuration, IServiceCollection services)
     {
+      var publicOptions = configuration.GetSection(RateLimiterSections.Public)
+        .Get<RateLimiterOptions>() ?? throw new ArgumentNullException(RateLimiterSections.Public);
+
+      var publicLoginOptions = configuration.GetSection(RateLimiterSections.PublicLogin)
+        .Get<RateLimiterOptions>() ?? throw new ArgumentNullException(RateLimiterSections.PublicLogin);
+
+      var authenticatedOptions = configuration.GetSection(RateLimiterSections.Authenticated)
+        .Get<RateLimiterOptions>() ?? throw new ArgumentNullException(RateLimiterSections.Authenticated);
+
       services.AddRateLimiter(limiterOptions =>
       {
         limiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
-        limiterOptions.AddPolicy(RateLimitingPolicies.Public, partitioner: httpContext =>
+        limiterOptions.AddPolicy(RateLimiterPolicies.Public, partitioner: httpContext =>
         {
           var ipAddress = httpContext.Connection.RemoteIpAddress;
           return RateLimitPartition.GetSlidingWindowLimiter(ipAddress, _ =>
