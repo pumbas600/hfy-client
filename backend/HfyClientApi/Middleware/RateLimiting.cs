c5ab2f54d92553dd2049363d8bca022aad037133
pumbas600 <joshjeffers600@gmail.com>
2024-10-08 23:23:48 +1300

    feat: create public rate limiter

---
+++ backend/HfyClientApi/Middleware/RateLimiting.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using System.Threading.RateLimiting;
+
+namespace HfyClientApi.Middleware
+{
+  public static class RateLimitingPolicies
+  {
+    public const string Public = "Public";
+    public const string PublicLogin = "PublicLogin";
+    public const string Authenticated = "Authenticated";
+  }
+
+  public class RateLimiting
+  {
+    public static void ConfigureRateLimiting(IServiceCollection services)
+    {
+      services.AddRateLimiter(limiterOptions =>
+      {
+        limiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+        limiterOptions.AddPolicy(RateLimitingPolicies.Public, partitioner: httpContext =>
+        {
+          var ipAddress = httpContext.Connection.RemoteIpAddress;
+          return RateLimitPartition.GetSlidingWindowLimiter(ipAddress, _ =>
+            new SlidingWindowRateLimiterOptions
+            {
+              PermitLimit = 50,
+              Window = TimeSpan.FromSeconds(60),
+              SegmentsPerWindow = 3,
+              QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+              QueueLimit = 5,
+            });
+        });
+      });
+    }
+  }
+}
