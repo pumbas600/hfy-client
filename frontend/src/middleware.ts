b7578503361be30ea59cab2b809a8601d73b63f6
pumbas600 <joshjeffers600@gmail.com>
2024-10-25 22:07:03 +1300

    fix: environment variables to use correct vercel variables

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,13 @@
 import appConfig from "@/config";
 import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+import serverConfig from "./config/serverConfig";
 
 export function middleware(request: NextRequest): NextResponse {
   const isAuthenticated = request.cookies.has(appConfig.cookies.refreshToken);
   console.debug(`[Middleware]: User is authenticated: ${isAuthenticated}`);
 
   if (!isAuthenticated) {
-    return NextResponse.redirect(appConfig.fontendBaseUrl + "/login");
+    return NextResponse.redirect(serverConfig.frontendUrl + "/login");
   }
 
   return NextResponse.next();
