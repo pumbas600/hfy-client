8de81600cd3472785be0aee9558fa1b3a8bdd5d1
pumbas600 <joshjeffers600@gmail.com>
2024-10-25 20:29:06 +1300

    feat: get frontend url from vercel environment variables

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -7,13 +7,24 @@ function requiredEnv(key: keyof NodeJS.ProcessEnv): string {
   return value;
 }
 
+function determineFrontendBaseUrl(): string {
+  if (process.env.NEXT_PUBLIC_FRONTEND_URL) {
+    return process.env.NEXT_PUBLIC_FRONTEND_URL;
+  }
+
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+    return `https://${requiredEnv("NEXT_PUBLIC_VERCEL_URL")}`;
+  }
+
+  return "http://localhost:3000";
+}
+
 const config = {
   api: {
     baseUrl:
       process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5070/api/v1",
   },
-  fontendBaseUrl:
-    process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000",
+  fontendBaseUrl: determineFrontendBaseUrl(),
   githubUrl: "https://github.com/pumbas600/hfy-client",
   discordInviteUrl: "https://discord.com",
   title: "HFY Client",
