2c319fba1596412879578c9bcefaa8975851a6ba
pumbas600 <joshjeffers600@gmail.com>
2024-10-26 14:24:21 +1300

    style: change hover and active styles to be outline

--- frontend/src/components/composite/homeIcon/index.tsx
+++ frontend/src/components/composite/homeIcon/index.tsx
@@ -11,9 +11,19 @@ export default function HomeIcon({
   hideTitle = false,
   ...props
 }: HomeIconProps) {
+  const appIconClasses = [styles.appIcon];
+
+  if (props.inverted) {
+    appIconClasses.push(styles.inverted);
+  }
+
+  if (props.className) {
+    appIconClasses.push(props.className);
+  }
+
   return (
     <Link href="/" className={styles.homeIcon}>
-      <AppIcon {...props} />
+      <AppIcon {...props} className={appIconClasses.join(" ")} />
       {!hideTitle && <p>{config.title}</p>}
     </Link>
   );
