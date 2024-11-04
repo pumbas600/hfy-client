5898cc88d4c7c304ea0e9962a6f9b7d42a3e7ec7
pumbas600 <joshjeffers600@gmail.com>
2024-11-04 16:43:49 +1300

    feat: close modal when clicking on backdrop

--- frontend/src/components/atomic/modal/index.tsx
+++ frontend/src/components/atomic/modal/index.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, ReactNode } from "react";
+import { forwardRef, MouseEvent, ReactNode } from "react";
 import styles from "./modal.module.css";
 import { cx } from "@/util/classNames";
 
@@ -10,16 +10,25 @@ interface ModalProps {
 
 const Modal = forwardRef<HTMLDialogElement, ModalProps>(
   ({ onClose, children, className }, ref) => {
+    const handleClick = (e: MouseEvent) => {
+      if (ref && "current" in ref && ref.current === e.target) {
+        onClose();
+      }
+    };
+
     return (
       <dialog
         ref={ref}
         onClose={onClose}
+        onClick={handleClick}
         className={cx(styles.modal, className)}
       >
-        {children}
+        <div className={styles.modalContent}>{children}</div>
       </dialog>
     );
   }
 );
+
+Modal.displayName = "Modal";
 
 export default Modal;
