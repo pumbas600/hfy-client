e74668dfecc7a6cb009d0df2b549373b4177d25c
pumbas600 <joshjeffers600@gmail.com>
2024-11-05 21:36:10 +1300

    chore: use `cx` in page layout

--- frontend/src/components/layout/pageLayout/PageLayout.tsx
+++ frontend/src/components/layout/pageLayout/PageLayout.tsx
@@ -1,17 +1,11 @@
 import { ReactNode } from "react";
 import styles from "./pageLayout.module.css";
-import { Subtitle } from "@/components/atomic";
-import LinkList from "@/components/composite/linkList";
-import { DevelopmentLinks } from "@/config/constants";
-import Footer from "./footer";
-
+import { cx } from "@/util/classNames";
 export interface PageLayoutProps {
   children?: ReactNode;
   className?: string;
 }
 
 export default function PageLayout({ children, className }: PageLayoutProps) {
-  return (
-    <div className={`${styles.pageLayout} ${className ?? ""}`}>{children}</div>
-  );
+  return <div className={cx(styles.pageLayout, className)}>{children}</div>;
 }
