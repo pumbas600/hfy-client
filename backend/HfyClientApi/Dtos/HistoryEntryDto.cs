a2e5fb32fd4b3271bf232abb119a3a39fbb5f102
pumbas600 <joshjeffers600@gmail.com>
2024-11-22 23:48:03 +1300

    feat: create dtos

---
+++ backend/HfyClientApi/Dtos/HistoryEntryDto.cs
@@ -0,0 +1,13 @@
+namespace HfyClientApi.Dtos
+{
+  public class CreateHistoryEntryDto
+  {
+    public required string ChapterId { get; set; }
+  }
+
+  public class HistoryEntryDto : CreateHistoryEntryDto
+  {
+    public required string UserId { get; set; }
+    public DateTime ReadAtUtc { get; set; }
+  }
+}
