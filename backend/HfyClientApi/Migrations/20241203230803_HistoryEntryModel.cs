c0e79fc6384666954fe0df4e7c79d444e26f2c09
pumbas600 <joshjeffers600@gmail.com>
2024-12-04 12:29:45 +1300

    chore: create history entry migration

---
+++ backend/HfyClientApi/Migrations/20241203230803_HistoryEntryModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HfyClientApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class HistoryEntryModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HistoryEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ChapterId = table.Column<string>(type: "text", nullable: false),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    ReadAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistoryEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HistoryEntries_Chapters_ChapterId",
+                        column: x => x.ChapterId,
+                        principalTable: "Chapters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HistoryEntries_Users_UserName",
+                        column: x => x.UserName,
+                        principalTable: "Users",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HistoryEntries_ChapterId",
+                table: "HistoryEntries",
+                column: "ChapterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HistoryEntries_UserName",
+                table: "HistoryEntries",
+                column: "UserName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HistoryEntries");
+        }
+    }
+}
