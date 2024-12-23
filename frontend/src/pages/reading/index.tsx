121c8b209442c0a562954ff5f7354f932b07c06b
pumbas600 <joshjeffers600@gmail.com>
2024-12-23 15:35:05 +1300

    feat: get reading history in server side props

--- frontend/src/pages/reading/index.tsx
+++ frontend/src/pages/reading/index.tsx
@@ -1,8 +1,41 @@
 import { HeadMeta } from "@/components/atomic";
 import { Aside, Header, PageLayout } from "@/components/layout/pageLayout";
 import config from "@/config";
+import { GetReadingHistoryRequest } from "@/types/api";
+import { ChapterMetadata } from "@/types/chapter";
+import { Api } from "@/util/api";
+import { GetServerSideProps } from "next";
 
-export default function ReadingPage() {
+interface ReadingHistoryPageProps {
+  readingHistory: ChapterMetadata[];
+}
+
+export const getServerSideProps = (async ({ req, res }) => {
+  const readingHistoryUrl = `${config.api.baseUrl}/history/reading`;
+
+  try {
+    await Api.assertAccessTokenPresent(req, res);
+    const response = await Api.get<GetReadingHistoryRequest.ResBody>(
+      readingHistoryUrl,
+      { req }
+    );
+
+    return {
+      props: { readingHistory: response.data },
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      props: { readingHistory: [] },
+    };
+  }
+}) satisfies GetServerSideProps<ReadingHistoryPageProps>;
+
+export default function ReadingHistoryPage({
+  readingHistory,
+}: ReadingHistoryPageProps) {
+  console.log(readingHistory);
+
   return (
     <>
       <HeadMeta title={`Reading | ${config.title}`} />
--- frontend/src/types/api/history.ts
+++ frontend/src/types/api/history.ts
@@ -1,7 +1,14 @@
+import { ChapterMetadata } from "../chapter";
 import { HistoryEntry } from "../history";
 
 export namespace PostHistoryRequest {
   export type Params = never;
   export type ReqBody = Pick<HistoryEntry, "chapterId">;
   export type ResBody = HistoryEntry;
+}
+
+export namespace GetReadingHistoryRequest {
+  export type Params = never;
+  export type ReqBody = never;
+  export type ResBody = ChapterMetadata[];
 }
